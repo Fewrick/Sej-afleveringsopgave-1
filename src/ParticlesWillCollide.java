96427bd4c53d2cafeb720eb18c647f19f0130722
Fewrick <54175840+Fewrick@users.noreply.github.com>
2022-10-10 12:38:28 +0200

    Create ParticlesWillCollide.java
    Opgave 2.b

---
+++ src/ParticlesWillCollide.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class ParticlesWillCollide {
+    public int x;
+    public int y;
+
+    public ParticlesWillCollide(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static void main(String[] args) {
+    runSimulation(10, 3, 50);
+    }
+
+    public static void runSimulation(int n, int s, int t) {
+        Random r = new Random();
+        Particles[] p = new Particles[2];
+        for (int i = 0; i < p.length; i++)
+            p[i] = new Particles(r.nextInt(n), r.nextInt(n)); // initialize all particles with random position
+
+        System.out.println("n=" + n + " s=" + s + " t=" + t); // print starting conditions
+
+        for (int i = 1; i <= t; i++) { // t number of times
+            String out = "move " + i + ", positions ";
+            for (int j = 0; j < p.length; j++) {
+                p[j].move(r.nextInt(2 * s + 1) - s, r.nextInt(2 * s + 1) - s, n); // for all particles move a random
+                                                                                  // amount between -s and s (inclusive)
+                                                                                  // in x and y direction
+                out += "P" + (j + 1) + "=[" + p[j].x + ";" + p[j].y + "] "; // Format positions in string
+            }
+
+            System.out.println(out.substring(0, out.length() - 1));
+
+            if ((p[0].x == p[1].x) && (p[0].y == p[1].y)) { // if statement that checks wether the position of p1 is equal to that of p2
+                System.out.println("particles collide");
+                for (int j = i + 1; j <= t; j++) {
+                    out = "move " + j + ", positions ";
+                    p[0].move(r.nextInt(2 * s + 1) - s, r.nextInt(2 * s + 1) - s, n);
+                    out += "P" + 1 + "=[" + p[0].x + ";" + p[0].y + "] ";
+                    System.out.println(out.substring(0, out.length() - 1));
+                }
+                break;
+            }
+        }
+
+    }
+
+    public void move(int moveX, int moveY, int n) {
+        // move x
+        this.x += moveX;
+        // fit within borders of grid
+        this.x = Math.max(0, this.x);
+        this.x = Math.min(n - 1, this.x);
+
+        // move y
+        this.y += moveY;
+        this.y = Math.max(0, this.y);
+        this.y = Math.min(n - 1, this.y);
+    }
+}
